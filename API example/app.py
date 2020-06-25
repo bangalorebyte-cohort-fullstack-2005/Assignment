5ae2cf7f62d53760cb28383a407c12e65f767160
vivekj1795 <vivekj1795@gmail.com>
2020-06-25 20:50:12 +0530

    script

--- API example/app.py
+++ API example/app.py
@@ -1,19 +1,39 @@
 from flask import Flask , render_template , request 
-from flask import Flask , render_template , request 
 import requests
 import json
 import random
-app = Flask(__name__)
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column , Integer , String, VARCHAR
+from sqlalchemy.orm import sessionmaker
+import csv
 
+app = Flask(__name__)
+engine = create_engine('sqlite:///temp.db' , echo=True , connect_args={'check_same_thread':False})
+Base = declarative_base()
+Session = sessionmaker(bind=engine)
+
+class Storage(Base):
+    __tablename__ = "Storage"
+
+    id = Column(VARCHAR, primary_key=True)
+    name = Column(VARCHAR(100) , nullable=False)
+
+    def __repr__(self):
+        return f"{self.name} has commented {self.comment_text}"
+    
+Base.metadata.create_all(engine)
 
 @app.route("/")
 def index():
-    r = requests.get("https://www.googleapis.com/books/v1/volumes?q=" +
-                            random.choice('abcdefghijklmnopqrstuvwxzy') +
-                            "&maxResults=40&key=AIzaSyDtKGwk1aWRFQYwW3qVE7CxoaBfp5VhaYA")
-    r1 = r.json()
-    print(r1)
-    return render_template("index.html",books=r1)
+    with open('books.csv','r') as fin:
+        dr = csv.DictReader(fin)
+        for i in dr:
+            obj = Storage(id=i["id"], name=i["name "])
+            session.add(obj)
+            session.commit()
+    return render_template("index.html")
 
 if __name__ == "__main__":
+    session = Session()
     app.run(debug=True)
